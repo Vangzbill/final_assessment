c7eafe020612e5b6a7c05cec8f96aba99ba0a2d4
Vangzbill <mamadhan017@gmail.com>
2025-04-18 18:28:42 +0700

    feat: style chatbot toggle button with hover effects and fade-in animation

--- resources/views/components/button-chatbot.blade.php
+++ resources/views/components/button-chatbot.blade.php
@@ -1,6 +1,7 @@
 <!-- Chatbot Floating Button -->
 <div id="chatbot-toggle" class="position-fixed bottom-0 end-0 m-4 zindex-tooltip" style="z-index: 1050;">
-    <button class="btn btn-primary rounded-circle p-3 shadow" title="Tanya Chatbot">
+    <button class="btn btn-primary p-3 shadow" title="Tanya Chatbot">
+        <span class="d-none d-md-inline-block">Tanya Chatbot</span>
         <i class="bi bi-chat-dots fs-4"></i>
     </button>
 </div>
